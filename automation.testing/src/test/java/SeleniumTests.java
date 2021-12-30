a8103c5b3b1f99ff3c0714792cad729e4987be63
tealek.alnutt <tealek.alnutt@teales-mbp>
2021-12-29 21:26:36 -0500

    Add web driver test

--- automation.testing/src/test/java/SeleniumTests.java
+++ automation.testing/src/test/java/SeleniumTests.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -7,11 +8,13 @@ import org.testng.annotations.Test;
 public class SeleniumTests {
   @Test
   public void canLaunchWebDriver() {
-	  String driverPath = "/usr/bin/safaridriver";
-	  System.setProperty("webdriver.safari.driver", driverPath);
+	  String chromeDriverPath = "/Users/tealek.alnutt/Downloads/chromedriver";
+	  String edgeDriverPath = "/Users/tealek.alnutt/Downloads/edgedriver_mac64/msedgedriver";
+	  System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+	  System.setProperty("webdriver.edge.driver", edgeDriverPath);
 	  
-	  String url = "https:www.selenium.dev/";
-	  WebDriver driver = new SafariDriver();
+	  String url = "https://www.selenium.dev/";
+	  WebDriver driver = new EdgeDriver();
 	  
 	  driver.navigate().to(url);
 	  String currentURL = driver.getCurrentUrl();
