99917b46663a445e144e0e82110303d90998fb7d
tealek.alnutt <tealek.alnutt@teales-mbp>
2022-01-02 12:46:11 -0500

    Add test to read basses.csv into object

--- Java.IO/src/test/java/com/testautomation/utilities/JavaHashmap.java
+++ Java.IO/src/test/java/com/testautomation/utilities/JavaHashmap.java
@@ -1,8 +1,16 @@
 package com.testautomation.utilities;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 public class JavaHashmap {
 	
@@ -35,5 +43,35 @@ public class JavaHashmap {
 			}
 		}
 	}
-
+	
+	@Test
+	public void canHydrateObject() {
+		
+		String file = "/Users/tealek.alnutt/Downloads/basses.csv";
+		
+		try {
+			FileReader fileReader = new FileReader(file);
+		
+			CSVParser parser = new CSVParserBuilder() . withSeparator(',').build();
+			CSVReader csvReader = new CSVReaderBuilder(fileReader)
+								.withCSVParser(parser)
+								.build();	
+		
+			List<String[]> allData = csvReader.readAll();
+		
+			for (String[] row : allData) {
+				for (String cell : row) {
+					System.out.print(cell + "\t");
+				}
+				System.out.println();
+			}
+		}
+		catch (Exception e) {
+		e.printStackTrace();
+		}
+		return;
+		
+	}
+		
+		
 }
