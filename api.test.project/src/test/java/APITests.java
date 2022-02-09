ba41c2eeeaac2b5b354c75816ab4bef4a7621da8
tealek.alnutt <tealek.alnutt@teales-mbp>
2022-02-09 16:25:42 -0500

    Completed TestNg Project -pass parameters -annotations -group tests

--- api.test.project/src/test/java/APITests.java
+++ api.test.project/src/test/java/APITests.java
@@ -1,4 +1,6 @@
 import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -6,16 +8,20 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import org.json.simple.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 
 public class APITests {
 	WebDriver driver;
 
-	@Test
+	@Parameters({"response"})
+	@Test (groups="verify")
 	public void canVerifyEmailAddress() {
 		String expectedEmail = "janet.weaver@reqres.in";
 		RestAssured.baseURI = "https://reqres.in";
@@ -27,10 +33,10 @@ public class APITests {
 				.extract()
 				.response();
 		String actualEmail = response.jsonPath().getString("data.email");
-		Assert.assertEquals(actualEmail, expectedEmail);
+		AssertJUnit.assertEquals(actualEmail, expectedEmail);
 		System.out.println(Thread.currentThread().getId());
 	}
-	@Test
+	@Test(groups="verify")
 	public void canVerifyToken() {
 		String expectedToken = "QpwL5tke4Pnpja7X4";
 		RestAssured.baseURI = "https://reqres.in";
@@ -44,7 +50,7 @@ public class APITests {
 		String actualToken = response.jsonPath().getString("data.token");
 		System.out.println(Thread.currentThread().getId());
 	}
-	@Test
+	@Test(groups="verify")
 	public void canDelete() {
 		String expectedCode = "204";
 		RestAssured.baseURI = "https://reqres.in";
@@ -58,7 +64,7 @@ public class APITests {
 		String actualCode = response.jsonPath().getString("data.response");
 		System.out.println(Thread.currentThread().getId());
 	}
-	@Test
+	@Test(groups="update")
 	public void canPatch() {
 		JSONObject request = new JSONObject();
 		request.put("name", "morpheus2");
@@ -74,7 +80,7 @@ public class APITests {
 		updatedTimeStamp = updatedTimeStamp.substring(0, updatedTimeStamp.length() - 7);
 		System.out.println(Thread.currentThread().getId());
 	}
-	@Test
+	@Test(groups="update")
 	public void canPut() {
 		JSONObject request = new JSONObject();
 		request.put("name", "morpheus2");
@@ -88,7 +94,7 @@ public class APITests {
 		System.out.println(Thread.currentThread().getId());
 	}
 	
-	@Test
+	@Test(groups="deserialize")
 	public void canGetSingleUser() {
 		String expectedEmail = "janet.weaver@reqres.in";
 		RestAssured.baseURI = "https://reqres.in";
@@ -103,15 +109,23 @@ public class APITests {
 		System.out.println(Thread.currentThread().getId());
 		
 	}
+	
 	@BeforeMethod
 	public void beforeMethod() {
 		System.setProperty("webdriver.chrome.driver","/Users/tealek.alnutt/Downloads/chromedriver");
 		driver = new ChromeDriver();
 	}
-
 	@AfterMethod
 	public void afterMethod() {
 		this.driver.quit();
+	}
+	@BeforeSuite
+	public void dataSetup() {
+		System.out.println("Data Setup");
+	}
+	@AfterSuite
+	public void dataTeardown() {
+		System.out.println("Data Cleanup");
 	}
 
 }
